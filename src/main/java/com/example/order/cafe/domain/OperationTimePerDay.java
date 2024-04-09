09bc86ee11e6b5501afa3b922acbaa9c5335299d
sky1231234 <blueskyee52@gmail.com>
2024-04-09 16:35:54 +0900

    feat : 요일, 운영시간을 갖는OperationTimePerDay의 유효성 검사와 단위 테스트 추가

---
+++ src/main/java/com/example/order/cafe/domain/OperationTimePerDay.java
@@ -0,0 +1,28 @@
+package com.example.order.cafe.domain;
+
+import com.example.order.cafe.errorMsg.OperationTimePerDayErrorMsg;
+
+public class OperationTimePerDay {
+
+    private Days days;
+    private OperationTime operationTime;
+
+    public OperationTimePerDay(Days days, OperationTime operationTime){
+        validation(operationTime);
+        this.days = days;
+        this.operationTime = operationTime;
+    }
+
+    public void validation(OperationTime operationTime){
+        if(operationTime == null){
+            throw new IllegalArgumentException(OperationTimePerDayErrorMsg.OPERATION_TIME_NON_NULL_ERROR_MSG.getValue());
+        }
+    }
+    public Days getDays(){
+        return this.days;
+    }
+
+    public OperationTime getOperationTime(){
+        return this.operationTime;
+    }
+}
