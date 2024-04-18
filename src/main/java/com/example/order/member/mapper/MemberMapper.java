b96285d623ea5e5b3c762dc4d82bda4a5de2164d
sky1231234 <blueskyee52@gmail.com>
2024-04-19 04:16:53 +0900

    feat : MemberService 메소드 분리, mapper final로 선언 수정

--- src/main/java/com/example/order/member/mapper/MemberMapper.java
+++ src/main/java/com/example/order/member/mapper/MemberMapper.java
@@ -3,13 +3,11 @@ package com.example.order.member.mapper;
 import com.example.order.member.domain.Member;
 import com.example.order.member.dto.response.MemberInfoResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
-    MemberMapper INSTANCE = Mappers.getMapper(MemberMapper.class);
 
     MemberInfoResponse toMemberInfoResponse(Member member);
     List<MemberInfoResponse> toMemberInfoResponseList(List<Member> member);
