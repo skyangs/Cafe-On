e9bf3bdc490b9e9ad25e66cb30a7f339297fa3ec
sky1231234 <blueskyee52@gmail.com>
2024-04-19 04:15:19 +0900

    feat : OperationTimePerDay Service, Request, Response 추가

--- src/main/java/com/example/order/cafe/repository/OperationTimePerDayRepository.java
+++ src/main/java/com/example/order/cafe/repository/OperationTimePerDayRepository.java
@@ -1,10 +1,14 @@
 package com.example.order.cafe.repository;
 
+import com.example.order.cafe.domain.BusinessHours;
 import com.example.order.cafe.domain.OperationTimePerDay;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OperationTimePerDayRepository extends JpaRepository<OperationTimePerDay, Long> {
 
+    List<OperationTimePerDay> findByBusinessHours(BusinessHours businessHours);
 }
