dcf49883e8398b568f09bd4f297ea0d0814c59e1
sky1231234 <blueskyee52@gmail.com>
2024-05-08 00:48:46 +0900

    feat: 메소드 별로 HTTP 요청 응답인 ApiResponse 추가

--- src/main/java/com/example/order/member/controller/MemberController.java
+++ src/main/java/com/example/order/member/controller/MemberController.java
@@ -6,6 +6,8 @@ import com.example.order.member.dto.request.UpdateMemberRequest;
 import com.example.order.member.dto.response.MemberResponse;
 import com.example.order.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +26,11 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/{id}")
-    @Operation(summary = "회원 정보 조회", description = "특정 회원의 정보를 조회한다.")
+    @Operation(summary = "회원 정보 조회", description = "특정 회원의 정보를 조회한다.",
+            responses = {
+            @ApiResponse(responseCode = "200", description = "회원 정보 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "회원 정보 조회 실패")
+    })
     public ResponseEntity<MemberResponse> getMemberInfo(@PathVariable("id") long id){
 
         return ResponseEntity.ok()
@@ -32,7 +38,11 @@ public class MemberController {
     }
 
     @GetMapping("/all")
-    @Operation(summary = "회원 정보 전체 조회", description = "모든 회원의 정보를 조회한다.")
+    @Operation(summary = "회원 정보 전체 조회", description = "모든 회원의 정보를 조회한다.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "회원 정보 전체 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "회원 정보 전체 조회 실패")
+    })
     public ResponseEntity<List<MemberResponse>> getMemberInfoList(){
 
         return ResponseEntity.ok()
@@ -40,7 +50,11 @@ public class MemberController {
     }
 
     @PostMapping("/signup")
-    @Operation(summary = "회원가입", description = "새로운 회원의 정보를 등록한다.")
+    @Operation(summary = "회원가입", description = "새로운 회원의 정보를 등록한다.",
+    responses = {
+            @ApiResponse(responseCode = "201", description = "회원 정보 등록 성공"),
+            @ApiResponse(responseCode = "404", description = "회원 정보 등록 실패")
+    })
     public ResponseEntity<Object> signup(@Validated @RequestBody SignUpRequest signUpRequest){
         Member member = memberService.signUp(signUpRequest.getMemberId(),
                 signUpRequest.getPassword(),
@@ -52,7 +66,11 @@ public class MemberController {
     }
 
     @PatchMapping("/{id}")
-    @Operation(summary = "회원 정보 수정", description = "특정 회원의 정보를 수정한다.")
+    @Operation(summary = "회원 정보 수정", description = "특정 회원의 정보를 수정한다.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "회원 정보 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "회원 정보 조회 실패")
+        })
     public ResponseEntity<Object> updateMemberInfo(@PathVariable("id") long id, @Validated @RequestBody UpdateMemberRequest updateMemberRequest){
 
         memberService.updateMember(id,
@@ -64,7 +82,11 @@ public class MemberController {
     }
 
     @DeleteMapping("/{id}")
-    @Operation(summary = "회원 탈퇴", description = "회원의 정보를 삭제한다.")
+    @Operation(summary = "회원 탈퇴", description = "회원의 정보를 삭제한다.",
+        responses = {
+            @ApiResponse(responseCode = "204", description = "회원 정보 삭제 성공"),
+            @ApiResponse(responseCode = "404", description = "회원 정보 삭제 실패")
+    })
     public ResponseEntity<Object> deleteMember(@PathVariable("id") long id){
 
         memberService.deleteMember(id);
